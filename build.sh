d1a4a0d6e2337d9163631b4213c3453e99372160
Starry Jerry <starry90@qq.com>
2019-02-21 18:18:36 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,40 +2,37 @@
 # File Name: build.sh
 # 注意:jenkins上用的是dash不是bash 语法上有区别!!!!
 #########################################################################
-#!/bin/sh
-set -u
-set -e
+#! /bin/sh
+set -u #使用未赋值的变量中断脚本执行
+set -e #任何语句的执行结果不是true则中断脚本执行
 
 #项目根目录
 project_path=`pwd`
 #最终输出目录
-out_path=$project_path/outputs/
+out_path="$project_path/outputs/"
 
-buildApk()
-{
-    echo ">> 编译开始"
+echo ">>> Shell build apk start"
+rm -rf $out_path
+mkdir $out_path
 
-    if [ ! -d $out_path ]; then
-        mkdir $out_path
-    else
-        cd $out_path
-        rm -rf *
-    fi
+#增加执行权限
+chmod +x gradlew
+#clean
+./gradlew clean
+#打包所有渠道
+./gradlew assemble
+echo "copy *.apk, *.jar and mapping.txt to "$out_path
+cp app/build/outputs/apk/*/*.apk $out_path
+cp app/build/outputs/mapping/*/mapping.txt $out_path
+./gradlew Http:makeJar
 
-    cd $project_path
-    #增加执行权限
-    chmod +x gradlew
-    #clean
-    ./gradlew clean
-    #打包Release
-    ./gradlew assemble
-    #-r表示递归处理，将指定目录下的文件与子目录一并处理。
-    #若源文件或目录的形态，不属于目录或符号链接，则一律视为普通文件处理
-    echo "cp *.apk and mapping.txt to "$out_path
-    cp app/build/outputs/apk/*/*.apk $out_path
-    cp app/build/outputs/mapping/*/mapping.txt $out_path
-    echo "<< 编译成功"
-    exit
-}
+cd $out_path
+du -b *.apk *.jar >> build-info.txt
+echo "" >> build-info.txt
+md5sum *.apk *.jar >> build-info.txt
 
-buildApk
+git_branch=`git symbolic-ref --short -q HEAD`
+git_version=`git rev-list HEAD --count`
+touch "${git_branch}-${git_version}"
+
+echo ">>> Shell build apk end"
