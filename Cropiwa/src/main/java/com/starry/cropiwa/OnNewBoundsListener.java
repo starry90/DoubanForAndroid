af8ecbed66ebe8759e3b25346d8f4366ff8391e8
Starry Jerry <starry90@qq.com>
2019-04-02 18:46:26 +0800

    Add Cropiwa module

---
+++ Cropiwa/src/main/java/com/starry/cropiwa/OnNewBoundsListener.java
@@ -0,0 +1,11 @@
+package com.starry.cropiwa;
+
+import android.graphics.RectF;
+
+/**
+ * Created by yarolegovich https://github.com/yarolegovich
+ * on 06.02.2017.
+ */
+interface OnNewBoundsListener {
+    void onNewBounds(RectF bounds);
+}
