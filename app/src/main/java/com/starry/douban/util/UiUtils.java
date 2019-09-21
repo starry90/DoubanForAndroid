1ad5585b0b80707ad9e3320600809ec1b2d56afa
Starry Jerry <starry90@qq.com>
2019-09-21 18:00:08 +0800

    add dialog util method

--- app/src/main/java/com/starry/douban/util/UiUtils.java
+++ app/src/main/java/com/starry/douban/util/UiUtils.java
@@ -1,7 +1,10 @@
 package com.starry.douban.util;
 
+import android.app.Dialog;
 import android.content.DialogInterface;
 import android.support.v4.content.ContextCompat;
+import android.view.Window;
+import android.view.WindowManager;
 
 import com.starry.douban.env.AppWrapper;
 
@@ -15,6 +18,38 @@ public class UiUtils {
     public static void dismissDialog(DialogInterface dialog) {
         if (dialog != null) {
             dialog.dismiss();
+        }
+    }
+
+    /**
+     * 设置对话框显示位置 <br>
+     * {@linkplain android.view.Gravity#BOTTOM} 显示在底部 <br>
+     * {@linkplain android.view.Gravity#TOP} 显示在顶部 <br>
+     *
+     * @param dialog  对话框
+     * @param gravity 对话框显示位置
+     */
+    public static void setGravity(Dialog dialog, int gravity) {
+        Window window = dialog.getWindow();
+        if (window != null) {
+            window.setGravity(gravity);
+        }
+    }
+
+    /**
+     * 设置对话框宽度占满全屏<br>
+     * https://blog.csdn.net/sydMobile/article/details/83588708
+     *
+     * @param dialog 对话框
+     */
+    public static void setMatchWidth(Dialog dialog) {
+        Window window = dialog.getWindow();
+        if (window != null) {
+            // 把 DecorView 的默认 padding 取消，同时 DecorView 的默认大小也会取消
+            window.getDecorView().setPadding(0, 0, 0, 0);
+            WindowManager.LayoutParams lp = window.getAttributes();
+            lp.width = WindowManager.LayoutParams.MATCH_PARENT;
+            window.setAttributes(lp);
         }
     }
 
