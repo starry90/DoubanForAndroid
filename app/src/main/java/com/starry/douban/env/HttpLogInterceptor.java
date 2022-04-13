2d3f64dd124e80cd69a661a55e0a73810da269f0
Jerry Starry <starry90@qq.com>
2022-04-13 11:50:29 +0800

    修复okhttp3.ResponseBody.string()多次调用错误（java.lang.IllegalStateException: closed）

--- app/src/main/java/com/starry/douban/env/HttpLogInterceptor.java
+++ app/src/main/java/com/starry/douban/env/HttpLogInterceptor.java
@@ -13,7 +13,9 @@ import okhttp3.Request;
 import okhttp3.RequestBody;
 import okhttp3.Response;
 import okhttp3.ResponseBody;
+import okhttp3.internal.Util;
 import okio.Buffer;
+import okio.BufferedSource;
 
 
 /**
@@ -31,25 +33,22 @@ public class HttpLogInterceptor implements Interceptor {
         // Request
         // --> https://...
         // --> GET
-        // --> ......
-        // --> ......
+        // --> {...}
         String url = request.url().toString();
         String requestLog = String.format("Request%n --> %s%n --> %s%n --> %s%n --> %s"
                 , url
                 , request.method()
                 , request.headers()
-                , request.body() != null ? parseParams(request.newBuilder().build().body()) : "Null");
-        Logger.d(TAG, requestLog);
+                , request.body() != null ? parseParams(request.newBuilder().build().body()) : "");
+        Logger.i(TAG, requestLog);
 
         Response originalResponse;
+        String responseLog;
         try {
             originalResponse = chain.proceed(request);
         } catch (Exception exception) {
-            String bodyString = "HTTP FAILED:" + exception;
-            String responseLog = String.format("Response%n >>> %s%n >>> %s"
-                    , url
-                    , bodyString);
-            Logger.d(TAG, responseLog);
+            responseLog = String.format("Response%n >>> %s%n >>> %s", url, exception.toString());
+            Logger.e(TAG, responseLog);
             throw exception;
         }
 
@@ -59,11 +58,11 @@ public class HttpLogInterceptor implements Interceptor {
         // >>> https://...
         // >>> ......
         // >>> {"code":0,...}
-        String responseLog = String.format("Response%n >>> %s%n >>> %s%n >>> %s"
+        responseLog = String.format("Response%n >>> %s%n >>> %s%n >>> %s"
                 , url
                 , originalResponse.headers()
                 , bodyString);
-        Logger.d(TAG, responseLog);
+        Logger.i(TAG, responseLog);
 
         return originalResponse;
     }
@@ -75,8 +74,15 @@ public class HttpLogInterceptor implements Interceptor {
         String bodyString = "无法解析";
         try {
             ResponseBody responseBody = response.body();
-            if (responseBody != null && isParsed(responseBody.contentType())) {
-                bodyString = responseBody.string();
+            MediaType contentType;
+            if (responseBody != null && isParsed(contentType = responseBody.contentType())) {
+                //okhttp3.ResponseBody.string()多次调用会报如下错
+                //java.lang.IllegalStateException: closed
+                BufferedSource source = responseBody.source();
+                source.request(Long.MAX_VALUE);
+                Buffer buffer = source.buffer();
+                Charset charset = Util.bomAwareCharset(source, contentType.charset(StandardCharsets.UTF_8));
+                bodyString = buffer.clone().readString(charset);
             }
         } catch (IOException e) {
             e.printStackTrace();
@@ -87,23 +93,19 @@ public class HttpLogInterceptor implements Interceptor {
 
 
     /**
-     * 解析请求服务器的请求参数
+     * 解析请求参数
      *
      * @param body RequestBody
      * @return 请求参数
      */
     public static String parseParams(RequestBody body) {
-        if (isParsed(body.contentType())) {
+        MediaType contentType = body.contentType();
+        if (isParsed(contentType)) {
             try {
                 Buffer requestBuffer = new Buffer();
                 body.writeTo(requestBuffer);
-                Charset charset = StandardCharsets.UTF_8;
-                MediaType contentType = body.contentType();
-                if (contentType != null) {
-                    charset = contentType.charset(charset);
-                }
+                Charset charset = contentType.charset(StandardCharsets.UTF_8);
                 return URLDecoder.decode(requestBuffer.readString(charset), convertCharset(charset));
-
             } catch (IOException e) {
                 e.printStackTrace();
             }
