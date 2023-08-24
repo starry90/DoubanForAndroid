54a9c50b8e5a2bcba3dd641587d2d91b9d894b1e
Jerry Starry <starry90@qq.com>
2023-08-24 17:37:19 +0800

    修复脚本错误导致jenkis构建失败

--- build.sh
+++ build.sh
@@ -4,7 +4,18 @@
 #########################################################################
 #! /bin/sh
 set -u #使用未赋值的变量中断脚本执行
+# 脚本有一处报处会导致jenkins构建失败
+# -e意味着，即使只有一个命令失败，shell脚本也会退出，如果您对该命令执行错误检查，则会退出
 set -e #任何语句的执行结果不是true则中断脚本执行
+
+# 问题出现：
+# Jenkins一直都构建成功，今天突然报错：Jenkins Build step 'Execute shell' marked build as failure
+# 问题原因：
+# By default Jenkins take /bin/sh -xe and this means -x will print each and every command.And the other option -e,
+# which causes shell to stop running a script immediately when any command exits with non-zero (when any command fails) exit code.
+# 解决办法：
+# 通过问题原因基本确定是因为-e或者-x导致构建失败(我这边是因为-x导致磁盘空间不足)，可以选择修复脚错误、或设置set +e
+# https://cloud.tencent.com/developer/ask/sof/96444
 
 #项目根目录
 project_path=`pwd`
@@ -24,7 +35,7 @@ chmod +x gradlew
 echo "copy *.apk, *.jar and mapping.txt to "$out_path
 cp app/build/outputs/apk/*/*.apk $out_path
 cp app/build/outputs/mapping/*/mapping.txt $out_path
-./gradlew Http:makeJar
+# ./gradlew Http:makeJar
 
 cd $out_path
 du -b *.apk >> build-info.txt
@@ -32,8 +43,7 @@ echo "" >> build-info.txt
 md5sum *.apk >> build-info.txt
 git log -n 10 --stat >> git-log.txt
 
-git_branch=`git symbolic-ref --short -q HEAD`
 git_version=`git rev-list HEAD --count`
-touch "${git_branch}-${git_version}"
+touch "git-${git_version}"
 
 echo ">>> Shell build apk end"
